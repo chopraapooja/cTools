6e08e88c51d9d8898feffa45ca85610a71a95664
Pooja Chopra <poojar@thoughtworks.com>
2015-01-15 18:48:48 +0530

    relloc syntex problem solved "index of element" is also accessible in predicate function

--- filter.c
+++ filter.c
@@ -1,13 +1,13 @@
-int filterIntegers(int *array, int length, int *filter(int), int **result){
-	int i, required, count=0;
-	 (*result) = (int *)0;
-	for (i = 0; i < length; ++i)
-	{
-		required = (*filter)(array[i]);
-		if(required){
-			 *result = (int *)realloc(*result, sizeof(int));
-			(*result)[count++] = array[i];
+int filterIntegers(int *array, int length, int predicate(int,int), int **result){
+	int i, yes, count=0;
+	int* filtered = 0;
+	for (i = 0; i < length; ++i){
+		yes = predicate(array[i],i);
+		if(yes){
+			filtered = (int *)realloc(filtered, (count+1)*sizeof(int));
+			filtered[count++] = array[i];
 		}
 	}
+	*result = filtered;
 	return count;
 }
--- filterTest.c
+++ filterTest.c
@@ -1,26 +1,43 @@
 #include <stdio.h>
 #include <assert.h>
 
-void test_filter_______________________________________________________________________________(){
-}
-
 void test_filter_On_providing_predicate_isEven_filterIntegers_should_filter_even_numbers_from_sample_array(){
 	int i;
 	int sample[] = {2, 7, 8 ,6, 6, 1};
 	int *filteredIntegers;
 	int expected[] = {2,8,6,6};
+
 	int isEven(int number){
 		return number%2 ? 0 : 1;
 	}
+
 	int number_of_filteredIntegers = filterIntegers(sample, 6, isEven, &filteredIntegers);
 	assert(number_of_filteredIntegers == 4);
-	for (i = 0; i < number_of_filteredIntegers; ++i)
-	{
+	for (i = 0; i < number_of_filteredIntegers; ++i){
 		assert(filteredIntegers[i] == expected[i]);
 	}
 	free(filteredIntegers);
 }
-	
+
+void test_filter_Predicate_should_be_able_to_access_element_and_its_index(){
+	int i;
+	int sample[] = {2, 7, 8 ,6, 6, 1};
+	int *filteredIntegers;
+	int expected[] = {7,6,1};
+
+	int isEvenTerm(int number, int index){
+		return (index+1)%2 ? 0 : 1;
+	}
+
+	int number_of_filteredIntegers = filterIntegers(sample, 6, isEvenTerm, &filteredIntegers);
+
+	assert(number_of_filteredIntegers == 3);
+	for (i = 0; i < number_of_filteredIntegers; ++i){
+		assert(filteredIntegers[i] == expected[i]);
+	}
+	free(filteredIntegers);
+}
+
 void test_filter_During_filtering_filterIntegers_should_not_modify_input_array(){
 	int i,sample[] = {2, 7, 8 ,6, 6, 1};
 	int sampleBackup[] = {2, 7, 8 ,6, 6, 1};
@@ -32,11 +49,5 @@ void test_filter_During_filtering_filterIntegers_should_not_modify_input_array()
 	int number_of_filteredIntegers =filterIntegers(sample, 6, isEven, &filteredIntegers);
 	assert(number_of_filteredIntegers == 4);
 	for (i = 0; i < 6; ++i)
-	{
 		assert(sampleBackup[i] == sample[i]);
-	}
-}
-
-void test_filter_End___________________________________________________________________________(){
-	printf("\n\n");
 }
