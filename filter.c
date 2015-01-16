e5c71b751ad8f47fb6403dff3455c928ce554f1e
Pooja Chopra <poojar@thoughtworks.com>
2015-01-16 16:45:52 +0530

    Revert "Added filterStrings"
    This reverts commit 41bdc0383ab85e0bf43b7edd8933fa6db6f2c30a.

--- filter.c
+++ filter.c
@@ -1,5 +1,3 @@
-#include "cTools.h"
-
 int filterIntegers(int *array, int length, int predicate(int,int), int **result){
 	int i, yes, count=0;
 	int* filtered = 0;
@@ -8,20 +6,6 @@ int filterIntegers(int *array, int length, int predicate(int,int), int **result)
 		if(yes){
 			filtered = (int *)realloc(filtered, (count+1)*sizeof(int));
 			filtered[count++] = array[i];
-		}
-	}
-	*result = filtered;
-	return count;
-}
-
-int filterStrings(char** strings, int length, predicateStr p, char ***result){
-	int i, yes, count=0;
-	char** filtered = 0;
-	for (i = 0; i < length; ++i){
-		yes = p(strings[i],i);
-		if(yes){
-			filtered = (char **)realloc(filtered, (count+1)*10);
-			filtered[count++] = strings[i];
 		}
 	}
 	*result = filtered;
